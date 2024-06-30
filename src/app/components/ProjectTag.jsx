e78813aefbbcea7a4304bb54bca4d6280c4c26b6
Diogo Gonçalves <a101919@alunos.uminho.pt>
2024-06-30 01:53:02 +0100

    gaps on mobile

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -5,13 +5,12 @@ const ProjectTag = ({ name, onClick, isSelected }) => {
     ? "text-white border-primary-500"
     : "text-[#ADB7BE] border-slate-600 hover:border-white";
 
-  // Adjust these classes as needed for responsive layout
-  const containerClasses = "flex items-center justify-center";
+  // Estilos para o botão
   const buttonClasses =
-    "rounded-full border-2 px-6 py-3 text-xl cursor-pointer";
+    "rounded-full border-2 px-4 md:px-6 py-2 md:py-3 text-lg md:text-xl cursor-pointer";
 
   return (
-    <div className={`m-2 ${containerClasses}`}>
+    <div className="m-2 flex items-center justify-center">
       <button
         className={`${buttonStyles} ${buttonClasses} md:min-w-[150px]`}
         onClick={() => onClick(name)}
